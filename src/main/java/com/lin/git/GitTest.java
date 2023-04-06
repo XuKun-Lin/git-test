2361aaeb630b043e73d99a50323eac1cbc840847
XuKunLin <96012563+XuKun-Lin@users.noreply.github.com>
2023-04-06 22:44:00 +0800

    3

--- src/main/java/com/lin/git/GitTest.java
+++ src/main/java/com/lin/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lin.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world1");
+        System.out.println("hello world3");
     }
 }
